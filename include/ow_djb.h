3570cc83831aad4693612c7da9a0a11dc9018dfc
Andrew Livesay <andrew.livesay@gmail.com>
2012-12-27 07:10:14 +0000

    using seed implicitly

--- include/ow_djb.h
+++ include/ow_djb.h
@@ -4,13 +4,12 @@
 
 /* Daniel J. Bernstein's DJBX33A */
 
-#define OW_DJB_DEFAULT_SEED  5381
+#define OW_DJB_SEED  5381
 
 inline unsigned int
-ow_djb_hash_str(const char *p_key,
-                const unsigned int p_seed)
+ow_djb_hash_str(const char *p_key)
 {
-  unsigned int hash = p_seed;
+  unsigned int hash = OW_DJB_SEED;
   
   while (*p_key)
     hash = ((hash << 5) + hash) + *p_key++;
@@ -20,10 +19,9 @@ ow_djb_hash_str(const char *p_key,
 
 
 inline unsigned int
-ow_djb_hash_str_16(const char *p_key,
-                   const unsigned int p_seed)
+ow_djb_hash_str_16(const char *p_key)
 {
-  unsigned int hash = ow_djb_hash_str(p_key, p_seed);
+  unsigned int hash = ow_djb_hash_str(p_key);
 
   return (hash ^ (hash >> 16));
 }
@@ -31,10 +29,9 @@ ow_djb_hash_str_16(const char *p_key,
 
 inline unsigned int
 ow_djb_hash(const char *p_key,
-            const unsigned int p_seed,
             unsigned int p_len)
 {
-  unsigned int hash = p_seed;
+  unsigned int hash = OW_DJB_SEED;
 
   while (p_len--)
     hash = ((hash << 5) + hash) + p_key[p_len];
@@ -45,20 +42,18 @@ ow_djb_hash(const char *p_key,
 
 inline unsigned int
 ow_djb_hash_16(const char *p_key,
-               const unsigned int p_seed,
                unsigned int p_len)
 {
-  unsigned int hash = ow_djb_hash(p_key, p_seed, p_len);
+  unsigned int hash = ow_djb_hash(p_key, p_len);
 
   return hash ^ (hash >> 16);
 }
 
 
 inline unsigned int
-ow_djb_hash_str_v2(const char *p_key,
-                   const unsigned int p_seed)
+ow_djb_hash_str_v2(const char *p_key)
 {
-  unsigned int hash = p_seed;
+  unsigned int hash = OW_DJB_SEED;
 
   while (*p_key)
     hash = ((hash << 5) + hash) ^ *p_key++;
@@ -68,10 +63,9 @@ ow_djb_hash_str_v2(const char *p_key,
 
 
 inline unsigned int
-ow_djb_hash_str_16_v2(const char *p_key,
-                      const unsigned int p_seed)
+ow_djb_hash_str_16_v2(const char *p_key)
 {
-  unsigned int hash = ow_djb_hash_str_v2(p_key, p_seed);
+  unsigned int hash = ow_djb_hash_str_v2(p_key);
 
   return hash ^ (hash >> 16);
 }
@@ -79,10 +73,9 @@ ow_djb_hash_str_16_v2(const char *p_key,
 
 inline unsigned int
 ow_djb_hash_v2(const char *p_key,
-               const unsigned int p_seed,
                unsigned int p_len)
 {
-  unsigned int hash = p_seed;
+  unsigned int hash = OW_DJB_SEED;
 
   while (p_len--)
     hash = ((hash << 5) + hash) ^ p_key[p_len];
@@ -93,10 +86,9 @@ ow_djb_hash_v2(const char *p_key,
 
 inline unsigned int
 ow_djb_hash_16_v2(const char *p_key,
-                  const unsigned int p_seed,
                   unsigned int p_len)
 {
-  unsigned int hash = ow_djb_hash_v2(p_key, p_seed, p_len);
+  unsigned int hash = ow_djb_hash_v2(p_key, p_len);
 
   return hash ^ (hash >> 16);
 }
