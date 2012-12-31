3a52f2095a13855b2e4602a5c809b79826ef7490
Andrew Livesay <andrew.livesay@gmail.com>
2012-12-30 19:07:44 -0800

    added extra hash function types

--- include/ow_types.h
+++ include/ow_types.h
@@ -3,6 +3,9 @@
 
 typedef int(*OWCompareFunc)(void const *p_a, void const *p_b);
 typedef int(*OWFreeFunc)(void const *p_data);
+
 typedef unsigned int(*OWHashFunc)(char const *const p_string);
+typedef unsigned int(*OWHash32StrFunc)(const char *p_key); 
+typedef unsigned int(*OWHash32Func)(const char *p_key, unsigned int p_len); 
 
 #endif // OW_TYPES_H
