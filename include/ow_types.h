d50e4a72317b4f9961daff6169295365612cc92e
Andrew Livesay <andrew.livesay@gmail.com>
2012-12-30 19:17:30 -0800

    more left-hand const changes

--- include/ow_types.h
+++ include/ow_types.h
@@ -1,11 +1,13 @@
 #ifndef OW_TYPES_H
 #define OW_TYPES_H
 
-typedef int(*OWCompareFunc)(void const *p_a, void const *p_b);
-typedef int(*OWFreeFunc)(void const *p_data);
 
-typedef unsigned int(*OWHashFunc)(char const *const p_string);
+typedef int(*OWCompareFunc)(const void *p_a, const void *p_b);
+typedef int(*OWFreeFunc)(const void *p_data);
+
+typedef unsigned int(*OWHashFunc)(const char *const p_string);
 typedef unsigned int(*OWHash32StrFunc)(const char *p_key); 
 typedef unsigned int(*OWHash32Func)(const char *p_key, unsigned int p_len); 
+
 
 #endif // OW_TYPES_H
