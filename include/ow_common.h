33baddc055f1e20e54ff729d21dee33b3fd4b6cd
Andrew Livesay <andrew.livesay@gmail.com>
2012-12-30 19:02:36 -0800

    added pow2 functions

--- include/ow_common.h
+++ include/ow_common.h
@@ -1,12 +1,56 @@
 #ifndef OW_COMMON_H
 #define OW_COMMON_H
 
+#include <limits.h>
+#include <stdint.h>
+
+
+#define OW_ARRAY_SIZE(a)   (sizeof(a) / sizeof(a[0])
 
 #define OW_STRINGIZE_X(i)   #i
 #define OW_STRINGIZE(i)     OW_STRINGIZE_X(i)
 
 #define OW_MIN(a, b)   (((a) > (b)) ? (b) : (a))
 #define OW_MAX(a, b)   (((a) < (b)) ? (b) : (a))
+
+
+inline unsigned int
+next_power_of_two(unsigned int p_n, _Bool p_accept_current)
+{
+  unsigned int i;
+
+  if (p_n < 0) return 0;
+
+  if (!p_accept_current) p_n--;
+  
+  for (i = 0; i < sizeof(i) << 2; i <<= 1)
+    p_n |= (p_n >> i);
+  
+  return ++p_n;
+}
+
+
+inline unsigned int
+prev_power_of_two(unsigned int p_n, _Bool p_accept_current)
+{
+  unsigned int i;
+
+  if (p_n < 0) return 0;
+
+  if (!p_accept_current) p_n--;
+
+  for (i = 0; i < sizeof(i) << 2; i <<= 1)
+    p_n |= (x >> i);
+
+  return p_n - (p_n >> 1);
+}
+
+
+inline _Bool
+is_power_of_two(unsigned int p_n)
+{
+  return p_n > 0 && (p_n & (p_n - 1)) == 0;
+}
 
 
 #endif // OW_COMMON_H
--- src/ow_common.c
+++ src/ow_common.c
@@ -1,1 +1,10 @@
 #include "ow_common.h"
+
+
+extern unsigned int   next_power_of_two   (unsigned int p_n,
+                                           _Bool p_accept_current);
+
+extern unsigned int   prev_power_of_two   (unsigned int p_n,
+                                           _Bool p_accept_current);
+
+extern _Bool          is_power_of_two     (unsigned int p_n);
