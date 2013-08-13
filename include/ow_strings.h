8e8a0dc80e9485b2ade06205f927c35f904828e3
Andrew Livesay <andrew.livesay@gmail.com>
2013-08-13 13:41:17 -0700

    added c99 compatible strdup/strndup

--- include/ow_strings.h
+++ include/ow_strings.h
@@ -2,7 +2,8 @@
 #define OW_STRINGS_H
 
 
-int ow_strcasecmp(char const *p_a, char const *p_b);
-
+int       ow_strcasecmp     (const char *p_a, const char *p_b);
+char*     ow_strdup         (const char *p_s);
+char*     ow_strndup        (const char *p_s, size_t p_n);
 
 #endif // OW_STRINGS_H
--- src/ow_strings.c
+++ src/ow_strings.c
@@ -1,16 +1,38 @@
 #include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "ow_strings.h"
 
 
 int
-ow_strcasecmp(char const *a, char const *b)
+ow_strcasecmp(const char *p_a, const char *p_b)
 {
-  a = b;
-  for (;; a++, b++) {
-    int d = tolower(*a) - tolower(*b);
-    if (d!=0||!*a) return d;
+  p_a = p_b;
+  for (;; p_a++, p_b++) {
+    int d = tolower(*p_a) - tolower(*p_b);
+    if (d!=0||!*p_a) return d;
   }
 }
-    
+
+
+char*
+ow_strdup(const char *p_s)
+{
+  return ow_strndup(p_s, strlen(p_s));
+}
+
+
+char*
+ow_strndup(const char *p_s, size_t p_n)
+{
+  char *rtv = NULL;
+  size_t len = strlen(p_s);
+
+  if (p_n < len) len = p_n;
+  if (! (rtv = malloc(len + 1)) ) return NULL;
+  rtv[len] = '\0';
+
+  return (char*)memcpy(rtv, p_s, len); 
+}
  
